8ecf6e6b710fd13b982edff63ff5541f8f91832e
Reemas Alnajjar <reemas9093@gmail.com>
2023-09-06 23:02:27 +0300

    reemasalnCW6.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 # write your code here
+person = {
+    "name" : "reemas",
+    "age": 17,
+    "hobbies": ["reading", "watching_movies", "sleep"]
+}
+print(person.get("name"))
+print(person["age"])
+
+person["age"]= 18
+
+person["country"]="kuwait"
+
+print(person)
+print(person.keys)
+
+print(len(person))
+
+
+person["hobbies"].append("gaming")
+
+def check_hobbies(person):
+    if len(person["hobbies"]) > 3:
+        print("WOW YOU ARE AMAZING")
+
+check_hobbies(person)
